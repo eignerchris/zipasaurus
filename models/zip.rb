957739060d893120c9190ecef765d4400f10a799
Chris Eigner <eignerchris@gmail.com>
2013-04-26 21:42:04 -0700

    added donate button; removed dino logo

--- models/zip.rb
+++ models/zip.rb
@@ -9,14 +9,4 @@ class Zip
   property :state,      String, key: true
   property :state_full, String
   property :county,     String, key: true
-
-  # def to_json(options={})
-  #   { code: code,
-  #     lat: lat,
-  #     lng: lng,
-  #     city: city,
-  #     state: state,
-  #     state_full: state_full,
-  #     county: county }
-  # end
 end
