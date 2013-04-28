0d72b8c15a2726aee9f99f8a4283452484513cf4
Chris Eigner <eignerchris@gmail.com>
2013-04-27 20:38:43 -0700

    to plusses

--- app.rb
+++ app.rb
@@ -34,12 +34,12 @@ class App < ZipasaurusApp
 
     if params[:state].length > 2
       state = params[:state].capitalize
-      city  = params[:city].titleize.gsub("%20", ' ')
+      city  = params[:city].titleize.gsub("+", ' ')
 
       records = Zip.all(state_full: state, city: city)
     else
       state = params[:state].upcase
-      city  = params[:city].titleize.gsub("%20", ' ')
+      city  = params[:city].titleize.gsub("+", ' ')
       records = Zip.all(state: state, city: city)
     end
 
@@ -51,12 +51,12 @@ class App < ZipasaurusApp
     
     if params[:state].length > 2
       state  = params[:state].capitalize
-      county = params[:county].titleize.gsub("%20", ' ')
+      county = params[:county].titleize.gsub("+", ' ')
 
       records = Zip.all(state_full: state, county: county)
     else
       state  = params[:state].upcase
-      county = params[:county].titleize.gsub("%20", ' ')
+      county = params[:county].titleize.gsub("+", ' ')
 
       records = Zip.all(state: state, county: county)
     end
