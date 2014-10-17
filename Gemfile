3214ce3243ec98b73d37bfde93d9fbc5bb38cfda
Chris Eigner <eignerchris@gmail.com>
2014-10-16 17:08:29 -0700

    puma

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 source 'https://rubygems.org'
-ruby '1.9.2'
+ruby '2.1.2'
 
 gem 'rake'
 gem 'sinatra'
-gem 'rack-jsonp', :require => 'rack/jsonp'
+gem 'rack-cors'
 gem 'dm-core'
 gem 'dm-aggregates'
 gem 'dm-migrations'
@@ -11,8 +11,8 @@ gem 'dm-serializer'
 gem 'dm-timestamps'
 gem 'dm-types'
 gem 'dm-postgres-adapter'
-gem 'unicorn'
+gem 'puma'
 gem 'sentry-raven'
 gem 'newrelic_rpm'
 gem 'i18n'
-gem 'active_support', :require => ['active_support/lazy_load_hooks', 'active_support/core_ext/string']
+gem 'activesupport', :require => ['active_support/lazy_load_hooks', 'active_support/core_ext/string']
--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ class ZipasaurusApp < Sinatra::Base
 
   configure :development do
     use Rack::ShowExceptions
-    DataMapper.setup(:default, 'postgres://eignerchris:@localhost/zipasaurus')
+    DataMapper.setup(:default, 'postgres://root:@localhost/zipasaurus')
     DataMapper.auto_upgrade!
   end
 
@@ -22,7 +22,12 @@ class ZipasaurusApp < Sinatra::Base
   
   use Rack::CommonLogger
   use Rack::MethodOverride
-  use Rack::JSONP
+  use Rack::Cors do
+    allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => :get
+    end
+  end
 
   STATE_ABBREV = { "wyoming"=>"wy", "oregon"=>"or", "missouri"=>"mo", "arkansas"=>"ar", "new jersey"=>"nj", "north carolina"=>"nc", "illinois"=>"il", "indiana"=>"in", "tennessee"=>"tn", "west virginia"=>"wv", "ohio"=>"oh", "oklahoma"=>"ok", "michigan"=>"mi", "mississippi"=>"ms", "arizona"=>"az", "kentucky"=>"ky", "california"=>"ca", "minnesota"=>"mn", "kansas"=>"ks", "district of columbia"=>"dc", "massachusetts"=>"ma", "alaska"=>"ak", "virginia"=>"va", "north dakota"=>"nd", "wisconsin"=>"wi", "maryland"=>"md", "palau"=>"pw", "south dakota"=>"sd", "vermont"=>"vt", "nevada"=>"nv", "texas"=>"tx", "colorado"=>"co", "hawaii"=>"hi", "rhode island"=>"ri", "connecticut"=>"ct", "pennsylvania"=>"pa", "new york"=>"ny", "louisiana"=>"la", "washington"=>"wa", "south carolina"=>"sc", "alabama"=>"al", "nebraska"=>"ne", "new hampshire"=>"nh", "new mexico"=>"nm", "georgia"=>"ga", "delaware"=>"de", "utah"=>"ut", "montana"=>"mt", "idaho"=>"id", "iowa"=>"ia", "maine"=>"me", "florida"=>"fl", "id"=>"id", "ia"=>"ia", "tx"=>"tx", "il"=>"il", "in"=>"in", "tn"=>"tn", "ga"=>"ga", "de"=>"de", "ne"=>"ne", "nh"=>"nh", "nm"=>"nm", "ny"=>"ny", "nv"=>"nv", "nd"=>"nd", "dc"=>"dc", "nj"=>"nj", "nc"=>"nc", "al"=>"al", "vt"=>"vt", "ak"=>"ak", "va"=>"va", "ks"=>"ks", "az"=>"az", "ky"=>"ky", "ar"=>"ar", "sc"=>"sc", "sd"=>"sd", "fl"=>"fl", "pa"=>"pa", "pw"=>"pw", "me"=>"me", "mt"=>"mt", "ct"=>"ct", "co"=>"co", "md"=>"md", "ma"=>"ma", "ca"=>"ca", "mn"=>"mn", "mi"=>"mi", "ms"=>"ms", "mo"=>"mo", "ut"=>"ut", "hi"=>"hi", "ri"=>"ri", "oh"=>"oh", "ok"=>"ok", "or"=>"or", "la"=>"la", "wa"=>"wa", "wi"=>"wi", "wv"=>"wv", "wy"=>"wy" }
 
